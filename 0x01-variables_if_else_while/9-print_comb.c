6aaa045f8a1c5fb8d921f7a861e88268fc841252
Betabravah <betabravah@gmail.com>
2022-04-09 00:42:42 +0300

    commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int order = '0';
+
+	while (order <= '9')
+	{
+		putchar(order);
+		if (order != 9)
+		{
+			putchar(',');
+			putchar(' ')
+		}
+		order++;
+	}
+	putchar('\n');
+	return (0);
+}
