c8c5b8c945e8f8cfc53b0df5bfda7aee955dbcd2
Betabravah <betabravah@gmail.com>
2022-04-08 23:47:21 +0300

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int order1 = 97;
+	int order2 = 65;
+
+	while (order1 < 123)
+	{
+		putchar(order1);
+		orde1r++;
+	}
+		while (order1 < 92)
+	{
+		putchar(order1);
+		orde1r++;
+	}
+	putchar('\n');
+	return (0);
+}
+
+
