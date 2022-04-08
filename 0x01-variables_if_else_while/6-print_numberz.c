7fc65d9278e8db7e7e1483a7dfcb85c5fd5c91be
Betabravah <betabravah@gmail.com>
2022-04-09 00:13:06 +0300

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int order = 0;
+
+	while (order <= 9)
+	{
+		putchar(order);
+		order++;
+	}
+	putchar('\n');
+	return (0);
+}
