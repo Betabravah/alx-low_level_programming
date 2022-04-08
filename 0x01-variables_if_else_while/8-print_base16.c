fa1301a3eae631736efde9ba579f8ff5d0ddefa2
Betabravah <betabravah@gmail.com>
2022-04-09 00:24:34 +0300

    commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int order = 48;
+
+	while (order <= 58)
+	{
+		putchar(order);
+		order++;
+	}
+	while (order2 <= 102)
+	{
+		putchar(order2);
+		order2++;
+	}
+	putchar('\n');
+	return (0);
+}
+
